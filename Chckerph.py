8cc5da01ee0bf427585869c6dc764826e6491ca1
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-17 19:31:22 +0430

    Add files via upload

--- Chckerph.py
+++ Chckerph.py
@@ -1,13 +1,26 @@
-from phonenumbers import parse
-from phonenumbers import geocoder
-from phonenumbers import carrier
-import os
-import datetime
-from colorama import Fore
-import random
-from tqdm import tqdm
-import time
+# Check the requirements
+try:
+    from phonenumbers import parse
+    from phonenumbers import geocoder
+    from phonenumbers import carrier
+    import os
+    from os import system
+    import datetime
+    from colorama import Fore
+    import random
+    from tqdm import tqdm
+    import time
+except ImportError:
+    system("pip install phonenumbers ")
+    system("pip install os ")
+    system("pip install datetime ")
+    system("pip install colorama ")
+    system("pip install random ")
+    system("pip install tqdm ")
+    system("pip install time ")
+    exit("\n\nRun script Again")
 
+# Start APP   
 os.system("cls")
 print(Fore.RED+ """______________                         ______________            ______              
 ___  __ \__  /___________________      __  ____/__  /_______________  /______________
@@ -42,9 +55,11 @@ print(Fore.GREEN+ "Ture"+Fore.RESET,'\n')
 time.sleep(5)
 print("Almost Completed...",'\n')
 time.sleep(3)
-
-number = parse(unumber)
-print("Region/Location:",'\n')
-print(Fore.LIGHTMAGENTA_EX+ geocoder.description_for_number(number,'en')+Fore.RESET)
-print('\n',"Operator:",'\n')
-print(Fore.YELLOW+carrier.name_for_number(number,'en')+Fore.RESET)
+try:
+    number = parse(unumber)
+    print(" Region/Location:",'\n')
+    print(Fore.LIGHTMAGENTA_EX+ geocoder.description_for_number(number,'en')+Fore.RESET)
+    print('\n',"Operator:",'\n')
+    print(Fore.YELLOW+carrier.name_for_number(number,'en')+Fore.RESET)
+except:
+    print("Someting Went Wrong")
